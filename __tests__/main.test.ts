a6155d33dacb301aeb9dddaf1b2142ce80092d7b
Brenden Ehlers <brenden@theehlers.org>
2024-02-27 10:15:18 -0700

    added actual test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,25 +6,42 @@
 //  * variables following the pattern `INPUT_<INPUT_NAME>`.
 //  */
 
-// import * as core from '@actions/core'
-// import * as main from '../src/main'
+import * as core from '@actions/core'
+import * as main from '../src/main'
 
 // // Mock the action's main function
-// const runMock = jest.spyOn(main, 'run')
+const runMock = jest.spyOn(main, 'run')
 
 // // Other utilities
 // const timeRegex = /^\d{2}:\d{2}:\d{2}/
 
 // // Mock the GitHub Actions core library
-// let debugMock: jest.SpiedFunction<typeof core.debug>
-// let errorMock: jest.SpiedFunction<typeof core.error>
-// let getInputMock: jest.SpiedFunction<typeof core.getInput>
-// let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
-// let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
+let debugMock: jest.SpiedFunction<typeof core.debug>
+let errorMock: jest.SpiedFunction<typeof core.error>
+let getInputMock: jest.SpiedFunction<typeof core.getInput>
+let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
+let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
 
 describe('action', () => {
-  it('works', () => {
-    expect(1 === 1).toBeTruthy()
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    debugMock = jest.spyOn(core, 'debug').mockImplementation()
+    errorMock = jest.spyOn(core, 'error').mockImplementation()
+    getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
+    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
+    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+  })
+
+  it('sets the output text', async () => {
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    expect(setOutputMock).toHaveBeenNthCalledWith(
+      1,
+      'message',
+      expect.stringContaining('hello, actions')
+    )
   })
 })
 
