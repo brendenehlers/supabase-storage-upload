6f47aaf4a18e2e9c5359abcdb8cd9dddb7576c90
Brenden Ehlers <brenden@theehlers.org>
2024-02-27 10:19:12 -0700

    run all

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -16,20 +16,20 @@ const runMock = jest.spyOn(main, 'run')
 // const timeRegex = /^\d{2}:\d{2}:\d{2}/
 
 // // Mock the GitHub Actions core library
-let debugMock: jest.SpiedFunction<typeof core.debug>
-let errorMock: jest.SpiedFunction<typeof core.error>
-let getInputMock: jest.SpiedFunction<typeof core.getInput>
-let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
+// let debugMock: jest.SpiedFunction<typeof core.debug>
+// let errorMock: jest.SpiedFunction<typeof core.error>
+// let getInputMock: jest.SpiedFunction<typeof core.getInput>
+// let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
 let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
 
 describe('action', () => {
   beforeEach(() => {
     jest.clearAllMocks()
 
-    debugMock = jest.spyOn(core, 'debug').mockImplementation()
-    errorMock = jest.spyOn(core, 'error').mockImplementation()
-    getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
-    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
+    // debugMock = jest.spyOn(core, 'debug').mockImplementation()
+    // errorMock = jest.spyOn(core, 'error').mockImplementation()
+    // getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
+    // setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
     setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
   })
 
